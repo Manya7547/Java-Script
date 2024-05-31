4975b637c5aaebc176bc85977f91d3dd9e57d82d
Manya7547 <52461524+Manya7547@users.noreply.github.com>
2024-05-30 18:12:39 -0700

    add comparison code and notes

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+//try to make the two variables should have the same datatype 
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//null is converted to 0 and then compared
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+//always gives false
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
