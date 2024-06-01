56c41b75fda8d442161ed6c24fdec87c2814ce59
Manya7547 <52461524+Manya7547@users.noreply.github.com>
2024-05-31 17:24:28 -0700

    add string notes and code

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,32 @@
+const name = "manya"
+const repoCount = 50
+
+//concatenated string - old syntax
+console.log(name + repoCount + "Value"); 
+
+//string interpolation using back ticks - injects variables
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const game = new String('volleyball');
+
+console.log(game[0]);
+console.log(game.__proto__); //?
+console.log(game.length);
+console.log(game.toUpperCase());
+console.log(game.charAt(2));
+console.log(game.indexOf('l'));
+
+const subGame = game.substring(0,6); //can't use -ve values
+console.log(subGame);
+
+const newName = game.slice(-8, 4); //counts pos from end
+console.log(newName);
+
+const name1 = " manya ";
+console.log(name1);
+console.log(name1.trim());  //removes end spaces 
+
+//replace "%20" inside url with a "-" (real world example)
+const url = "https://manya.com/manya%20sohal"
+console.log(url.replace('%20','-'));
+
--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -43,3 +43,28 @@ console.log(typeof myFunction); //datatype is a function
 Is JS statically language or dynamically typed language? 
 -> JavaScript is a dynamically typed language.
 */
+
+/*
+2 memories - stack and heap
+- primitive types stored in stack. 
+- non-primitive types stored in heap
+*/
+
+let name1 = "Manya Sohal" 
+let name2 = name1;  //a copy of name1 is assigned to name2
+name2 = "Manan Khaneja" //here the copy is changed not the actual value
+console.log(name1);
+console.log(name2); 
+
+
+//this object is stored in heap
+let user1 = {
+    email: "msohal@scu.edu",
+    upi:"msohal"
+};
+
+let user2 = user1; //here actual reference is given to user2 
+user2.email = "manya@gmail";  //so it will be changed for both objects
+console.log(user1.email);
+console.log(user2.email);
+
