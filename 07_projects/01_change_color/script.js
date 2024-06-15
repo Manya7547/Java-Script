fc1da24a17da7cf714d7705e84de34a77411aa08
Manya7547 <52461524+Manya7547@users.noreply.github.com>
2024-06-14 20:56:52 -0700

    background change project

---
+++ 07_projects/01_change_color/script.js
@@ -0,0 +1,20 @@
+// select the elements as node list so you can loop over them 
+
+const body = document.querySelector('body');
+const buttons = document.querySelectorAll('.button');
+console.log(buttons);
+
+buttons.forEach(function(button){
+    button.addEventListener('click', function(e){
+        // console.log(e); // 
+        // console.log(e.target); //shows where is this event coming from
+        if(e.target.id == 'grey')
+            body.style.backgroundColor = e.target.id;
+        if(e.target.id == 'blue')
+            body.style.backgroundColor = e.target.id;
+        if(e.target.id == 'yellow')
+            body.style.backgroundColor = e.target.id;
+        if(e.target.id == 'white')
+            body.style.backgroundColor = e.target.id;
+    })
+});
---
+++ 07_projects/projects_set1.md
@@ -0,0 +1,3 @@
+# Projects related to DOM
+
+[project link](https://stackblitz.com/edit/dom-project-chaiaurcode?file=index.html)
