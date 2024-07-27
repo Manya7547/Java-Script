7fd923704ee224324dc12663359b80bd5144225e
Manya7547 <52461524+Manya7547@users.noreply.github.com>
2024-07-26 19:17:57 -0700

    keyboard event code

---
+++ 07_projects/06_keyboard/script.js
@@ -0,0 +1,22 @@
+// press a key and it will show what key was pressed
+// keydown event in JS - triggered when a key on the keyboard is pressed down. It allows you to execute a function or piece of code in response to the user pressing a key.
+// add an event listener to that particukar id - "insert"
+
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', function(e){
+    insert.innerHTML = `
+    <table>
+    <tr>
+        <th>Key</th>
+        <th>KeyCode</th>
+        <th>Code</th>
+    </tr>
+    <tr>
+        <td>${e.key}</td>
+        <td>${e.keyCode}</td>
+        <td>${e.code}</td>
+    </tr>
+    </table>
+    `;
+},false);
